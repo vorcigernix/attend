d2a0275927c3252a6a2a12166cc20604ffef34fc
Adam Sobotka <vorcigernix@gmail.com>
2023-01-13 19:28:48 +0100

    registerUX

--- src/routes/validate/+page.svelte
+++ src/routes/validate/+page.svelte
@@ -1,11 +1,12 @@
 <script>
 	import { generateAndWriteKeys } from '$lib/indexedDBUtil';
 	import { browser } from '$app/environment';
-	//import { userNameStore } from '$lib/localStore.js';
+	import { userInfo } from '$lib/localStore.js';
 	let name = '';
 	let eckey = null;
 	$: existingUser = false;
 	$: submitting = false;
+	const userName = $userInfo ? JSON.parse($userInfo.userdetails).nickname : null;
 	async function writeKey() {
 		submitting = true;
 		if (!browser || name === '') return;
@@ -62,7 +63,7 @@
 			class="flex flex-col justify-center p-6 text-center lg:max-w-md xl:max-w-lg lg:text-left backdrop-blur bg-black/40 rounded-3xl {submitting &&
 				`opacity-10`}"
 		>
-			{#if existingUser}
+			{#if existingUser || userName}
 				<h1 class="text-5xl font-bold leading-none sm:text-6xl typogra">
 					Have we
 					<span class="text-lime-400">met</span> ?
@@ -73,30 +74,37 @@
 					<span class="text-lime-400">restore</span> verification
 				</h1>
 			{/if}
-			<div class="flex flex-col space-y-2 mt-4">
-				<label for="username" class="text-zinc-200">Your name/Nickname</label>
+			{#if !userName}
+				<div class="flex flex-col space-y-2 mt-4">
+					<label for="username" class="text-zinc-200">Your name/Nickname</label>
 
-				<div>
-					<input
-						type="text"
-						name="username"
-						bind:value={name}
-						class="w-full border-zinc-200 p-4 pr-12  shadow-sm rounded-2xl text-zinc-900 mb-1"
-						placeholder="that'd be stored and used further on"
-						required
-					/>
+					<div>
+						<input
+							type="text"
+							name="username"
+							bind:value={name}
+							class="w-full border-zinc-200 p-4 pr-12  shadow-sm rounded-2xl text-zinc-900 mb-1"
+							placeholder="that'd be stored and used further on"
+							required
+						/>
 
-					<span class="text-sm text-zinc-500 mt-2"
-						>We recommend using distinctive, unique name - instead of 'Adam' use Adam S. 'Johny'
-						would be easier to recognize as 'JohnyBoy74'</span
-					>
+						<span class="text-sm text-zinc-500 mt-2"
+							>We recommend using distinctive, unique name - instead of 'Adam' use Adam S. 'Johny'
+							would be easier to recognize as 'JohnyBoy74'</span
+						>
+					</div>
 				</div>
-			</div>
+			{/if}
 			{#if existingUser}
 				<p class="mt-6 mb-8 text-lg sm:mb-12">
 					We already know a user with this nickname.
 					<br class="hidden md:inline lg:hidden" />If this is yours, please use restore button. If
 					not, consider adding your favorite number at the end.
+				</p>
+			{:else if userName}
+				<p class="mt-6 mb-8 text-lg sm:mb-12">
+					Hey {userName} nice to see you again.
+					<br class="hidden md:inline lg:hidden" />It is good to ve validated huh? How we can help?
 				</p>
 			{:else}
 				<p class="mt-6 mb-8 text-lg sm:mb-12">
@@ -106,33 +114,41 @@
 					any device.
 				</p>
 			{/if}
+
 			<div
 				class="flex flex-col space-y-4 sm:items-center sm:justify-center sm:flex-row sm:space-y-0 sm:space-x-4 lg:justify-start"
 			>
-				<button
-					on:click|preventDefault={() => writeKey()}
-					class=" font-bold inline-flex items-center text-zinc-900 bg-lime-500 border-0 py-2 px-6 focus:outline-none hover:bg-lime-600 rounded-full text-sm md:text-base"
-					>Create verification</button
-				><button
-					class=" font-bold ml-4 rounded-full inline-flex items-center text-zinc-200 bg-zinc-800 border-0 py-2 px-6 focus:outline-none hover:bg-zinc-700 hover:text-zinc-50 text-sm md:text-base"
-					>Restore</button
-				>
-				<a href="/about#auth" class="ml-4 rounded-full inline-flex items-center py-2 px-2">
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						class="w-6 h-6 text-zinc-200 hover:text-zinc-50"
-						fill="none"
-						viewBox="0 0 24 24"
-						stroke="currentColor"
-						stroke-width="2"
+				{#if !userName}
+					<button
+						on:click|preventDefault={() => writeKey()}
+						class=" font-bold inline-flex items-center text-zinc-900 bg-lime-500 border-0 py-2 px-6 focus:outline-none hover:bg-lime-600 rounded-full text-sm md:text-base"
+						>Create verification</button
+					><button
+						class=" font-bold ml-4 rounded-full inline-flex items-center text-zinc-200 bg-zinc-800 border-0 py-2 px-6 focus:outline-none hover:bg-zinc-700 hover:text-zinc-50 text-sm md:text-base"
+						>Restore</button
 					>
-						<path
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9 5.25h.008v.008H12v-.008z"
-						/></svg
-					></a
-				>
+					<a href="/about#auth" class="ml-4 rounded-full inline-flex items-center py-2 px-2">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							class="w-6 h-6 text-zinc-200 hover:text-zinc-50"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke="currentColor"
+							stroke-width="2"
+						>
+							<path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9 5.25h.008v.008H12v-.008z"
+							/></svg
+						></a
+					>
+				{:else}
+					<a
+						class=" font-bold inline-flex items-center text-zinc-900 bg-lime-500 border-0 py-2 px-6 focus:outline-none hover:bg-lime-600 rounded-full text-sm md:text-base"
+						href="mailto:vorcigernix@gmail.com">Something don't work?</a
+					>
+				{/if}
 			</div>
 		</div>
 	</div>
